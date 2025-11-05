0b9398b205d5b2fa0492d515e9cd41046316d73a
H.M.S.D.HERATH <sdulanjana2021@gmail.com>
2025-11-05 23:49:23 +0530

    add exercise card and details

--- lib/pages/add_new.dart
+++ lib/pages/add_new.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:workout_planner/constants/colors.dart';
+import 'package:workout_planner/constants/responsive.dart';
+import 'package:workout_planner/data/user_data.dart';
 
 class AddNew extends StatefulWidget {
   const AddNew({super.key});
@@ -8,13 +11,46 @@ class AddNew extends StatefulWidget {
 }
 
 class _AddNewState extends State<AddNew> {
+  //import the user from user data
+  final userData = user;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Center(
-            child: Text("Add new  page", style: TextStyle(fontSize: 40)),
+          child: Padding(
+            padding: const EdgeInsets.all(kDefaultPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Hello , ${userData.fullName}",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: kMainBlackColor,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Lets Add Some Workouts and Equipment for today!",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.normal,
+                    color: kMainColor,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "All Exercises",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: kMainBlackColor,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/widgets/add_exercise_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddExerciseCard extends StatefulWidget {
+  const AddExerciseCard({super.key});
+
+  @override
+  State<AddExerciseCard> createState() => _AddExerciseCardState();
+}
+
+class _AddExerciseCardState extends State<AddExerciseCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
